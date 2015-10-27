da6d52a903624f5cf4a98eb1f9b33800960ce194
wordtinker <wordtinker@yandex.ru>
2015-10-27 15:20:16 +0300

    .

---
+++ var_and_const/ConsoleApplication1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main()
+        {
+            const string title = "Some string."; // wont even compile if you try to assign
+            Console.Title = title;
+            var someOtherTitle = title; // takes the type from title
+            someOtherTitle += " !!!";
+            Console.Title = someOtherTitle;
+        }
+    }
+}
