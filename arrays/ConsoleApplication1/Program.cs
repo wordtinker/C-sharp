d378d8902506963b77af495a9281ce8031c832bf
wordtinker <wordtinker@yandex.ru>
2015-10-30 19:36:36 +0300

    .

---
+++ arrays/ConsoleApplication1/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main()
+        {
+            decimal[] prices = new decimal[2];
+            prices[0] = 36M;
+            prices[1] = 34M;
+
+            int[] items = new int[] { 4, 5 };
+            decimal totalPrice = prices[0] * items[0] + prices[1] * items[1];
+            Console.WriteLine("Total price is {0:C}", totalPrice);
+
+            // Multidimensional array
+            string[,] multi = new string[3, 5];
+            for (int i = 0; i < multi.GetLength(0); i++)
+            {
+                for (int j = 0; j < multi.GetLength(1); j++)
+                {
+                    var s = String.Format("[{0},{1}]", i, j);
+                    multi[i, j] = s;
+                    Console.Write(multi[i,j]);
+                }
+                Console.WriteLine();
+            }
+
+            //Proving reference type
+            Console.WriteLine("the price is {0}", prices[0]);
+            decimal[] new_prices = prices;
+            new_prices[0] = 356M;
+            Console.WriteLine("the price is {0}", prices[0]);
+        }
+    }
+}
---
+++ ref_out/ConsoleApplication1/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Mix(ref int param)
+        {
+            param = 1000;
+        }
+
+        static void ChangeArray(int[] arr)
+        {
+            arr[0] = 1000;
+            arr = new int[1] { 99999 };
+            Console.WriteLine("Inside the method the first element is {0}", arr[0]);
+        }
+
+        static void ChangeWithRef(ref int[] arr)
+        {
+            arr[0] = 1000;
+            arr = new int[1] { 99999 };
+            Console.WriteLine("Inside the method the first element is {0}", arr[0]);
+        }
+
+        static void Main()
+        {
+            int x = 1;
+            Console.WriteLine("Param is {0}", x);
+            Mix(ref x);
+            Console.WriteLine("Param is {0}", x);
+
+            int[] xArray = { 1, 1, 1, 1, 1 };
+            int[] anotherArray = xArray;
+            Console.WriteLine("Before the change the first element is {0}", xArray[0]);
+            ChangeArray(xArray);
+            Console.WriteLine("After the method the first element is {0}", xArray[0]);
+            Console.WriteLine("Array copy first element is {0}", anotherArray[0]);
+            Console.WriteLine("Arrays are same: {0}", (xArray == anotherArray));
+            Console.WriteLine();
+
+            Console.WriteLine("Before the change the first element is {0}", xArray[0]);
+            ChangeWithRef(ref xArray);
+            Console.WriteLine("After the method the first element is {0}", xArray[0]);
+            Console.WriteLine("Array copy first element is {0}", anotherArray[0]);
+            Console.WriteLine("Arrays are same: {0}", (xArray == anotherArray));
+
+        }
+    }
+}
