9def889894dc600bcccc5f1a2b459fc8ed156828
wordtinker <wordtinker@yandex.ru>
2015-11-01 16:28:40 +0300

    .

---
+++ classes_2/ConsoleApplication1/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Person
+    {
+        private static int personCounter = 0;
+
+        private string initials = null;
+
+        public Person(string init)
+        {
+            this.initials = init;
+            personCounter += 1;
+        }
+
+        public static int GetPersonCount()
+        {
+            return Person.personCounter;
+        }
+
+        public string Initials
+        {
+            get
+            {
+                Console.WriteLine("Getting initials.");
+                return initials;
+            }
+            set
+            {
+                Console.WriteLine("Setting initials to: {0}", value);
+                initials = value;
+            }
+            
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter initials:");
+            Person[] people = new Person[10];
+            for (int i = 0; Person.GetPersonCount() < people.Length; i++)
+            {
+                people[i] = new Person(Console.ReadLine());
+                people[i].Initials += "!";
+            }
+
+        }
+    }
+}
---
+++ classes_abstract/ConsoleApplication1/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    abstract class ThreeDShape
+    {
+        public abstract double GetVolume();
+    }
+
+    class Sphere : ThreeDShape
+    {
+        private double radius;
+
+        public Sphere(double r)
+        {
+            radius = r;
+        }
+
+        public override double GetVolume()
+        {
+            return 4.0 / 3.0 * Math.PI * Math.Pow(radius, 3);
+        }
+    }
+
+    class Cube: ThreeDShape
+    {
+        private double side;
+
+        public Cube(double s)
+        {
+            side = s;
+        }
+
+        public override double GetVolume()
+        {
+            return Math.Pow(side, 3);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ThreeDShape[] forms = { new Sphere(4), new Cube(8) };
+            foreach (var form in forms)
+            {
+                Console.WriteLine("The volume is {0}", form.GetVolume());
+            }
+
+        }
+    }
+}
