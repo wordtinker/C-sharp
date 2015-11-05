ce7675323b2c473cac98b0969e91c0947fd4a865
wordtinker <wordtinker@yandex.ru>
2015-11-05 12:31:32 +0300

    .

---
+++ reading_files/ConsoleApplication1/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.IO;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamWriter sw = new StreamWriter(@"c:\data\SampleFile.txt");
+            sw.Write(Console.ReadLine());
+            sw.Close();
+
+            StreamReader sr = new StreamReader(@"c:\data\SampleFile.txt");
+            Console.WriteLine(sr.ReadLine());
+            sr.Close();
+
+            // more modern approach
+            using (StreamWriter writer = File.CreateText(@"c:\data\SampleFile.txt"))
+            {
+                string s = Console.ReadLine();
+                while (s != "Exit")
+                {
+                    writer.Write(s+"\n");
+                    s = Console.ReadLine();
+                }
+            }
+
+            using (StreamReader reader = File.OpenText(@"c:\data\SampleFile.txt"))
+            {
+                string line;
+                while((line = reader.ReadLine())!= null)
+                {
+                    Console.WriteLine(line);
+                }
+            }
+            Console.WriteLine();
+
+
+
+
+        }
+    }
+}
