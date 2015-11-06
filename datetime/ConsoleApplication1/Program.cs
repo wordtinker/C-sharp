b4084b79943f272f4f30b599ec6d2227ba06b3e2
wordtinker <wordtinker@yandex.ru>
2015-11-06 12:12:48 +0300

    .

---
+++ datetime/ConsoleApplication1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Dates and times: ");
+            DateTime dt = new DateTime(2015, 11, 5);
+            Console.WriteLine("The day of {0} is {1}", dt.Date, dt.DayOfWeek);
+            dt = dt.AddMonths(1);
+            Console.WriteLine("The day of {0} is {1}", dt.Date, dt.DayOfWeek);
+
+            TimeSpan ts = new TimeSpan(1, 3, 0);
+            Console.WriteLine(ts);
+        }
+    }
+}
---
+++ string_builder/ConsoleApplication1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Text;
+
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StringBuilder sb = new StringBuilder("This is string builder.");
+            sb.Append(10);
+            sb.Append("text");
+            string substr = "substring";
+            sb.AppendFormat("And even format {0}", substr);
+            Console.WriteLine(sb.ToString());
+        }
+    }
+}
