eefd2a10a275bb409e12f0e0877f55ccd93ea622
wordtinker <wordtinker@yandex.ru>
2015-11-10 14:58:59 +0300

    .

---
+++ SimpleLambdaExpressions/ConsoleApplication1/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("***** Fun with Lambdas *****\n");
+            TraditionalDelegateSyntax();
+            AnonymousMethodSyntax();
+            LambdaExpressionsSyntax();
+            Console.ReadLine();
+        }
+
+        static void TraditionalDelegateSyntax()
+        {
+            List<int> list = new List<int>();
+            list.AddRange(new int[] { 20, 1, 4, 8, 9, 44 });
+
+            Predicate<int> callback = IsEvenNumber;
+            List<int> evenNumbers = list.FindAll(callback);
+
+            Console.WriteLine("Here are your even numbers:");
+            foreach (int i in evenNumbers)
+            {
+                Console.Write("{0}\t", i);
+            }
+            Console.WriteLine();
+
+        }
+
+        static bool IsEvenNumber(int i)
+        {
+            return (i % 2) == 0; 
+        }
+
+        static void AnonymousMethodSyntax()
+        {
+            // Make a list of integers.
+            List<int> list = new List<int>();
+            list.AddRange(new int[] { 20, 1, 4, 8, 9, 44 });
+            // Now, use an anonymous method.
+            List<int> evenNumbers = list.FindAll(delegate (int i)
+            {
+                return (i % 2) == 0;
+            });
+            Console.WriteLine("Here are your even numbers:");
+            foreach (int evenNumber in evenNumbers)
+            {
+                Console.Write("{0}\t", evenNumber);
+            }
+            Console.WriteLine();
+        }
+
+        static void LambdaExpressionsSyntax()
+        {
+            // Make a list of integers.
+            List<int> list = new List<int>();
+            list.AddRange(new int[] { 20, 1, 4, 8, 9, 44 });
+            // Now, use a C# lambda expression.
+            List<int> evenNumbers = list.FindAll(i => (i % 2) == 0);
+            Console.WriteLine("Here are your even numbers:");
+            foreach (int evenNumber in evenNumbers)
+            {
+                Console.Write("{0}\t", evenNumber);
+            }
+            Console.WriteLine();
+        }
+    }
+}
