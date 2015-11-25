0f4339ba1c1afa46589f3c4c955c18074dbaca34
wordtinker <wordtinker@yandex.ru>
2015-11-25 14:04:03 +0300

    .

---
+++ InventoryEDMConsoleApp/ConsoleApplication1/CarClass.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    public partial class Car
+    {
+        public override string ToString()
+        {
+            // Since the PetName column could be empty, supply
+            // the default name of **No Name**.
+            return string.Format("{0} is a {1} {2} with ID {3}.",
+            this.CarNickname ?? "**No Name**",
+            this.Color, this.Make, this.CarID);
+        }
+    }
+}
+
---
+++ InventoryEDMConsoleApp/ConsoleApplication1/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.Core;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("***** Fun with ADO.NET EF *****\n");
+            PrintAllInventory();
+            Console.WriteLine("***** Add new record to DB *****\n");
+            AddNewRecord();
+            UpdateRecord();
+            PrintAllInventory();
+            Console.ReadLine();
+
+        }
+
+        private static void PrintAllInventory()
+        {
+            using(AutoLotEntities context = new AutoLotEntities())
+            {
+                foreach (Car item in context.Cars)
+                {
+                    Console.WriteLine(item);
+                }
+            }
+        }
+        private static void AddNewRecord()
+        {
+            // Add record to the Inventory table of the AutoLot
+            // database.
+            using (AutoLotEntities context = new AutoLotEntities())
+            {
+                try
+                {
+                    // Hard-code data for a new record, for testing.
+                    context.Cars.Add(new Car()
+                    {
+                        CarID = 2222,
+                        Make = "Yugo",
+                        Color = "Brown"
+                    });
+                    context.SaveChanges();
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.InnerException.Message);
+                }
+            }
+        }
+        private static void UpdateRecord()
+        {
+            // Find a car to delete by primary key.
+            using (AutoLotEntities context = new AutoLotEntities())
+            {
+                Car carToUpdate = (from c in context.Cars
+                                  where c.CarID == 2222
+                                  select c).FirstOrDefault();
+
+                if (carToUpdate != null)
+                {
+                    carToUpdate.Color = "Blue";
+                    context.SaveChanges();
+                }
+            }
+        }
+    }
+}
