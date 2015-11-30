a25b208126f585dbca3588ebadcca9e9aec83b15
wordtinker <wordtinker@yandex.ru>
2015-11-30 10:30:06 +0300

    .

---
+++ WpfControlsAndAPIs/WpfControlsAndAPIs/MainWindow.xaml.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfControlsAndAPIs
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            // Be in Ink mode by default.
+            this.myInkCanvas.EditingMode = InkCanvasEditingMode.Ink;
+            this.inkRadio.IsChecked = true;
+            this.comboColors.SelectedIndex = 0;
+
+            PopulateDocument();
+            ConfigureGrid();
+        }
+
+        private void RadioButtonClicked(object sender, RoutedEventArgs e)
+        {
+            // Based on which button sent the event, place the InkCanvas in a unique
+            // mode of operation.
+            switch ((sender as RadioButton).Content.ToString())
+            {
+                case "Ink Mode!":
+                    this.myInkCanvas.EditingMode = InkCanvasEditingMode.Ink;
+                break;
+                case "Erase Mode!":
+                    this.myInkCanvas.EditingMode = InkCanvasEditingMode.EraseByStroke;
+                break;
+                case "Select Mode!":
+                    this.myInkCanvas.EditingMode = InkCanvasEditingMode.Select;
+                break;
+            }
+        }
+
+        private void ColorChanged(object sender, SelectionChangedEventArgs e)
+        {
+            // Get the selected value in the combo box.
+            string colorToUse =
+            (this.comboColors.SelectedItem as ComboBoxItem).Content.ToString();
+            // Change the color used to render the strokes.
+            this.myInkCanvas.DefaultDrawingAttributes.Color =
+            (Color)ColorConverter.ConvertFromString(colorToUse);
+        }
+
+        private void PopulateDocument()
+        {
+            // Add some data to the List item.
+            this.listOfFunFacts.FontSize = 14;
+            this.listOfFunFacts.MarkerStyle = TextMarkerStyle.Circle;
+            this.listOfFunFacts.ListItems.Add(new ListItem(new
+            Paragraph(new Run("Fixed documents are for WYSIWYG print ready docs!"))));
+            this.listOfFunFacts.ListItems.Add(new ListItem(
+            new Paragraph(new Run("The API supports tables and embedded figures!"))));
+            this.listOfFunFacts.ListItems.Add(new ListItem(
+            new Paragraph(new Run("Flow documents are read only!"))));
+            this.listOfFunFacts.ListItems.Add(new ListItem(new Paragraph(new Run
+            ("BlockUIContainer allows you to embed WPF controls in the document!")
+            )));
+            // Now add some data to the Paragraph.
+            // First part of sentence.
+            Run prefix = new Run("This paragraph was generated ");
+            // Middle of paragraph.
+            Bold b = new Bold();
+            Run infix = new Run("dynamically");
+            infix.Foreground = Brushes.Red;
+            infix.FontSize = 30;
+            b.Inlines.Add(infix);
+            // Last part of paragraph.
+            Run suffix = new Run(" at runtime!");
+            // Now add each piece to the collection of inline elements
+            // of the Paragraph.
+            this.paraBodyText.Inlines.Add(prefix);
+            this.paraBodyText.Inlines.Add(infix);
+            this.paraBodyText.Inlines.Add(suffix);
+        }
+
+        private class Auto
+        {
+            public string CarID { get; set; }
+            public string Make { get; set; }
+            public string Color { get; set; }
+            public string PetName { get; set; }
+        }
+
+        private void ConfigureGrid()
+        {
+            List<Auto> carList = new List<Auto>();
+            carList.Add(new Auto() { CarID = "1", Make = "BMW", Color = "Yellow", PetName = "Chuck" });
+            carList.Add(new Auto() { CarID = "1", Make = "BMW", Color = "Yellow", PetName = "Chuck" });
+            carList.Add(new Auto() { CarID = "1", Make = "BMW", Color = "Yellow", PetName = "Chuck" });
+
+            this.gridInventory.ItemsSource = carList;
+            
+        }
+    }
+}
---
+++ WpfWordPad/WpfWordPad/MainWindow.xaml.cs
@@ -0,0 +1,119 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfWordPad
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            SetF1CommonBinding();
+        }
+
+        private void SetF1CommonBinding()
+        {
+            CommandBinding helpBinding = new CommandBinding(ApplicationCommands.Help);
+            helpBinding.CanExecute += (sender, e) => e.CanExecute = true;
+            helpBinding.Executed += HelpBinding_Executed;
+            CommandBindings.Add(helpBinding);
+        }
+
+        private void HelpBinding_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+            MessageBox.Show("Look, it is not that difficult. Just type something!", "Help!");
+        }
+
+        private void FileExit_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void MouseEnterExitArea(object sender, MouseEventArgs e)
+        {
+            statBarText.Text = "Exit the Application";
+        }
+
+        private void MouseLeaveArea(object sender, MouseEventArgs e)
+        {
+            statBarText.Text = "Ready";
+        }
+
+        private void MouseEnterToolsHintsArea(object sender, MouseEventArgs e)
+        {
+            statBarText.Text = "Show Spelling Suggestions";
+        }
+
+        private void ToolsSpellingHints_Click(object sender, RoutedEventArgs e)
+        {
+            string spellingHints = string.Empty;
+            // Try to get a spelling error at the current caret location.
+            SpellingError error = txtData.GetSpellingError(txtData.CaretIndex);
+            if (error != null)
+            {
+                // Build a string of spelling suggestions.
+                foreach (string s in error.Suggestions)
+                {
+                    spellingHints += string.Format("{0}\n", s);
+                }
+                // Show suggestions and expand the expander.
+                lblSpellingHints.Content = spellingHints;
+                expanderSpelling.IsExpanded = true;
+            }
+        }
+
+        private void OpenCmdExecuted(object sender, ExecutedRoutedEventArgs e)
+        {
+            // Create an open file dialog box and only show XAML files.
+            OpenFileDialog openDlg = new OpenFileDialog();
+            openDlg.Filter = "Text Files |*.txt";
+            // Did they click on the OK button?
+            if (true == openDlg.ShowDialog())
+            {
+                // Load all text of selected file.
+                string dataFromFile = File.ReadAllText(openDlg.FileName);
+                // Show string in TextBox.
+                txtData.Text = dataFromFile;
+            }
+        }
+
+        private void OpenCmdCanExecute(object sender, CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = true;
+        }
+
+        private void SaveCmdExecuted(object sender, ExecutedRoutedEventArgs e)
+        {
+            SaveFileDialog saveDlg = new SaveFileDialog();
+            saveDlg.Filter = "Text Files |*.txt";
+            // Did they click on the OK button?
+            if (true == saveDlg.ShowDialog())
+            {
+                // Save data in the TextBox to the named file.
+                File.WriteAllText(saveDlg.FileName, txtData.Text);
+            }
+        }
+
+        private void SaveCmdCanExecute(object sender, CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = true;
+        }
+    }
+}
