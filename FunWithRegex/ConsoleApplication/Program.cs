a3cbc1ad9fe586495273951b423a5a6ccf05060b
wordtinker <wordtinker@yandex.ru>
2015-12-13 10:03:26 +0300

    .

---
+++ FunWithRegex/ConsoleApplication/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string word = "go";
+            string endOfWord = "$";
+            Regex re = new Regex(endOfWord);
+            // Add ending
+            string newWord = re.Replace(word, "ing");
+            Console.WriteLine(string.Format("{0}->{1}", word, newWord));
+
+            string endsWithO = "o$";
+            re = new Regex(endsWithO);
+            // Replace O With "reat"
+            newWord = re.Replace(word, "reat");
+            Console.WriteLine(string.Format("{0}->{1}", word, newWord));
+            // Strip the letter
+            newWord = re.Replace(word, "");
+            Console.WriteLine(string.Format("{0}->{1}", word, newWord));
+            // Keep the word untouched
+            newWord = re.Replace(word, "$&");
+            Console.WriteLine(string.Format("{0}->{1}", word, newWord));
+
+            // Starts ^ with any characters .+ Ends not with ab
+            string notAB = "^.+[^ab]$";
+            re = new Regex(notAB);
+            // Add ending
+            newWord = re.Replace(word, "$&ing");
+            Console.WriteLine(string.Format("{0}->{1}", word, newWord));
+            string wordEndsWithB = "blob";
+            newWord = re.Replace(wordEndsWithB, "$&ing");
+            Console.WriteLine(string.Format("{0}->{1}", word, newWord));
+        }
+    }
+}
