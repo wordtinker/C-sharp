0ebc12eec7f09b191bb9efeaa8c3e44406445bfa
wordtinker <wordtinker@yandex.ru>
2015-12-18 07:30:20 +0300

    .

---
+++ 1.Basics/Namespaces/Namespaces/Program.cs
@@ -0,0 +1,14 @@
+﻿using System; // Import every member from that namespace
+
+namespace Namespaces
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Use fully qualified name
+            // Also add this one to referenced assemblies.
+            System.Windows.Forms.MessageBox.Show("Hello World");
+        }
+    }
+}
