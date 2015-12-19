9c07d6188ca913e9e174fb2a22f862fee580e69f
wordtinker <wordtinker@yandex.ru>
2015-12-19 09:33:05 +0300

    .

---
+++ WPF/MVVMQuickExample/MVVMQuickExample/Views/MainWindow.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System.Windows;
+
+
+namespace MVVMQuickExample
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        // Notice that now we don't use control-specific events here.
+        // Use them as ModelView properties
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            // We have declared the view model instance declaratively in the xaml.
+            // Get the reference to it here, so we can use it in the button click event.
+            // _viewModel = (AlbumViewModel)base.DataContext;
+        }
+    }
+}
