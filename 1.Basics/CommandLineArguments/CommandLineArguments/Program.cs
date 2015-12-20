0c4fb50f48bc6c518455f403c5ab96959d6fe2cd
wordtinker <wordtinker@yandex.ru>
2015-12-20 11:45:42 +0300

    .

---
+++ 1.Basics/CommandLineArguments/CommandLineArguments/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace CommandLineArguments
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Process any incoming args.
+            for (int i = 0; i < args.Length; i++)
+                Console.WriteLine("Arg: {0}", args[i]);
+            Console.ReadLine();
+
+            // process with foreach
+            foreach (string arg in args)
+                Console.WriteLine("Arg: {0}", arg);
+            Console.ReadLine();
+
+            // Get arguments using System.Environment.
+            string[] theArgs = Environment.GetCommandLineArgs();
+            foreach (string arg in theArgs)
+                Console.WriteLine("Arg: {0}", arg);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ 1.Basics/ConsoleInput/ConsoleInput/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace ConsoleInput
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Input value: ");
+            string input = Console.ReadLine();
+            try
+            {
+                double inputValue = double.Parse(input);
+                Console.WriteLine(inputValue);
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Try better.");
+            }
+
+            // Using tryParse is better
+            Console.Write("Input value: ");
+            input = Console.ReadLine();
+            double iValue;
+            if (double.TryParse(input, out iValue))
+            {
+                Console.WriteLine(iValue);
+            }
+            else
+            {
+                Console.WriteLine("Try better.");
+            }
+        }
+    }
+}
