874329c07aa2b7c4ee50979ad6f213cf6427be55
wordtinker <wordtinker@yandex.ru>
2015-12-21 07:55:24 +0300

    .

---
+++ WPF/MVVMvalidation/MVVMvalidation/Models/Product.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace MVVMvalidation.Models
+{
+    class Product : IDataErrorInfo
+    {
+        // Properties
+        public string ProductName { get; set; }
+        public int Width { get; set; }
+        public int Height { get; set; }
+
+        // Methods
+        public void Save()
+        {
+            // Stub.
+        }
+
+        // Validation logic
+        private string ValidateName()
+        {
+            if (String.IsNullOrEmpty(this.ProductName))
+                return "Product Name needs to be entered.";
+            else if (this.ProductName.Length < 5)
+                return "Product Name should have more than 5 letters.";
+            else
+                return String.Empty;
+        }
+
+        private string ValidateHeight()
+        {
+            if (this.Height <= 0)
+                return "Height should be greater than 0";
+            if (this.Height > this.Width)
+                return "Height should be less than Width.";
+            else
+                return String.Empty;
+        }
+
+        private string ValidateWidth()
+        {
+            if (this.Width <= 0)
+                return "Width should be greater than 0";
+            if (this.Width < this.Height)
+                return "Width should be greater than Height.";
+            else
+                return String.Empty;
+        }
+
+        // IDataErrorInfo interface
+        /// <summary>
+        /// Gets an error message indicating what is wrong with this object.
+        /// </summary>
+        public string Error
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        /// <summary>
+        /// Gets the error message for the property with the given name.
+        /// </summary>
+        /// <param name="propertyName"></param>
+        /// <returns></returns>
+        public string this[string propertyName]
+        {
+            get
+            {
+                string validationResult = null;
+                switch (propertyName)
+                {
+                    case "ProductName":
+                        validationResult = ValidateName();
+                        break;
+                    case "Height":
+                        validationResult = ValidateHeight();
+                        break;
+                    case "Width":
+                        validationResult = ValidateWidth();
+                        break;
+                    default:
+                        throw new ApplicationException("Unknown Property being validated on Product.");
+                }
+                return validationResult;
+            }
+        }
+    }
+}
---
+++ WPF/MVVMvalidation/MVVMvalidation/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using MicroMvvm;
+using MVVMvalidation.Models;
+using System.Windows.Input;
+
+namespace MVVMvalidation.ViewModels
+{
+    class MainWindowViewModel : ObservableObject, IDataErrorInfo
+    {
+        // Members
+        private readonly Product currentProduct;
+        private Dictionary<string, bool> validProperties;
+        private bool allPropertiesValid = false;
+
+        // Properties
+        public string ProductName
+        {
+            get { return currentProduct.ProductName; }
+            set
+            {
+                if (currentProduct.ProductName != value)
+                {
+                    currentProduct.ProductName = value;
+                    RaisePropertyChanged("ProductName");
+                }
+            }
+        }
+
+        public int Width
+        {
+            get { return currentProduct.Width; }
+            set
+            {
+                if (currentProduct.Width != value)
+                {
+                    currentProduct.Width = value;
+                    RaisePropertyChanged("Width");
+                    RaisePropertyChanged("Height");
+                }
+            }
+        }
+
+        public int Height
+        {
+            get { return currentProduct.Height; }
+            set
+            {
+                if (currentProduct.Height != value)
+                {
+                    currentProduct.Height = value;
+                    RaisePropertyChanged("Height");
+                    RaisePropertyChanged("Width");
+                }
+            }
+        }
+
+        public bool AllPropertiesValid
+        {
+            get { return allPropertiesValid; }
+            set
+            {
+                if (allPropertiesValid != value)
+                {
+                    allPropertiesValid = value;
+                    RaisePropertyChanged("AllPropertiesValid");
+                }
+            }
+        }
+
+        // Constructors
+        public MainWindowViewModel()
+        {
+            currentProduct = new Product();
+            validProperties = new Dictionary<string, bool>();
+            validProperties.Add("ProductName", false);
+            validProperties.Add("Height", false);
+            validProperties.Add("Width", false);
+        }
+
+        // Methods
+        private void Save()
+        {
+            currentProduct.Save();
+        }
+
+        private void ValidateProperties()
+        {
+            foreach (bool isValid in validProperties.Values)
+            {
+                if (!isValid)
+                {
+                    this.AllPropertiesValid = false;
+                    return;
+                }
+            }
+            this.AllPropertiesValid = true;
+        }
+
+        // Commands
+        public ICommand SaveCommand
+        {
+            get { return new RelayCommand(() => Save(), () => true); }
+        }
+
+        // IDataErrorInfo interface
+        public string this[string propertyName]
+        {
+            get
+            {
+                string error = (currentProduct as IDataErrorInfo)[propertyName];
+                validProperties[propertyName] = String.IsNullOrEmpty(error);
+                ValidateProperties();
+                // Save command might be available after that,
+                // force.
+                CommandManager.InvalidateRequerySuggested();
+                return error;
+            }
+        }
+
+        public string Error
+        {
+            // Use model's method
+            get { return (currentProduct as IDataErrorInfo).Error; }
+        }
+    }
+}
