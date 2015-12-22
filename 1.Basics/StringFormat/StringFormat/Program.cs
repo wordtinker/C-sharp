ccff3013c92930846062c7d7635e55c9b92f7e35
wordtinker <wordtinker@yandex.ru>
2015-12-22 17:03:57 +0300

    .

--- 1.Basics/StringFormat/StringFormat/Program.cs
+++ 1.Basics/StringFormat/StringFormat/Program.cs
@@ -7,9 +7,16 @@ namespace StringFormat
         static void Main(string[] args)
         {
             Console.WriteLine("Print to the stdout: {0}", 25); //like python "".format() method
+            // C or c is used to format currency.
             Console.WriteLine("Use currency format: {0:C}", 25);
+            // Used for fixed-point formatting.
             Console.WriteLine("Use 4 decimal places: {0:F4}", 25.123456);
+            // Used for percent formatting.
             Console.WriteLine("Format with %: {0:P2}", 25.123456);
+            // Cant also be used as:
+            string userMessage = string.Format("100000 in hex is {0:x}", 100000);
+            Console.WriteLine(userMessage);
+
         }
     }
 }
