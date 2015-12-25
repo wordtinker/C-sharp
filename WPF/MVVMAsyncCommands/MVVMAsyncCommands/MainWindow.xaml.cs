ba775a8789c1ef49d572a264265e97a66edae1ad
wordtinker <wordtinker@yandex.ru>
2015-12-25 10:20:48 +0300

    .

---
+++ WPF/MVVMAsyncCommands/MVVMAsyncCommands/MainWindow.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System.Windows;
+
+
+namespace MVVMAsyncCommands
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        // How will the UI display errors?
+        // How should the UI look while the operation is in progress? (For example, will it provide immediate feedback via busy indicators?)
+        // How is the user restricted while the operation is in progress? (Are buttons disabled, for example?)
+        // Does the user have any additional commands available while the operation is in progress? (For instance, can he cancel the operation?)
+        // If the user can start multiple operations, how does the UI provide completion or error details for each one? (For example, will the UI use a “command queue” style or notification popups?)
+        // https://msdn.microsoft.com/en-us/magazine/dn630647.aspx
+        public MainWindow()
+        {
+            DataContext = new MainWindowViewModel();
+            InitializeComponent();
+        }
+    }
+}
---
+++ WPF/MVVMAsyncCommands/MVVMAsyncCommands/MainWindowViewModel.cs
@@ -0,0 +1,26 @@
+﻿using MicroMvvm;
+
+namespace MVVMAsyncCommands
+{
+    public sealed class MainWindowViewModel : ObservableObject
+    {
+        public MainWindowViewModel()
+        {
+            Url = "http://www.example.com/";
+            CountUrlBytesCommand = AsyncCommand.Create(() => MyService.DownloadAndCountBytesAsync(Url));
+        }
+
+        private string _url;
+        public string Url
+        {
+            get { return _url; }
+            set
+            {
+                _url = value;
+                RaisePropertyChanged("Url");
+            }
+        }
+
+        public IAsyncCommand CountUrlBytesCommand { get; private set; }
+    }
+}
