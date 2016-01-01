d5dcac4ac6044b1626b70b33c36416372d196fdd
wordtinker <wordtinker@yandex.ru>
2016-01-01 11:44:50 +0300

    .

---
+++ 1.Basics/Nullables/Nullables/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Nullables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Could be int or null
+            int? nullableInt = 10;
+            // Assume we get the value from DB.
+            nullableInt = null;
+
+            Console.WriteLine("The value is:{0}.", nullableInt.GetValueOrDefault());
+            Console.WriteLine("The value is:{0}.", nullableInt ?? 100); // Return 100 if null
+            Console.WriteLine("The value is:{0}.", nullableInt.GetValueOrDefault(100)); // Same
+        }
+    }
+}
