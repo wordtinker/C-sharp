dbf17018e56fa58bfc1b317a9662801977a2a36a
wordtinker <wordtinker@yandex.ru>
2016-01-06 09:56:03 +0300

    .

---
+++ 2.TypeSystem/Class/Constructors/Constructors/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace Constructors
+{
+    class Car
+    {
+        // The 'state' of the Car.
+        public string petName;
+        public int currSpeed;
+
+        // When custom constructor is declared, 
+        // default constructor is lost.
+
+        // A custom default constructor.
+        public Car()
+            : this("Chuck", 10) {} // Constructor chaining
+        // Here, currSpeed will receive the
+        // default value of an int (zero).
+        public Car(string pn)
+        {
+            petName = pn;
+        }
+        // Let caller set the full state of the Car.
+        public Car(string pn, int cs)
+        {
+            this.petName = pn; // Use of "this"
+            this.currSpeed = cs;
+        }
+
+        public void PrintState()
+        {
+            Console.WriteLine("{0} is going {1} MPH.", petName, currSpeed);
+        }
+    }
+
+    class OptCar
+    {
+        // The 'state' of the Car.
+        public string petName;
+        public int currSpeed;
+
+        // One Constructor instead of three.
+        public OptCar(string pn = "Chuck", int cs = 10)
+        {
+            petName = pn;
+            currSpeed = cs;
+        }
+    }
+
+        class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("***** Fun with Class Types *****\n");
+            // Make a Car called Chuck going 10 MPH.
+            Car chuck = new Car();
+            chuck.PrintState();
+            // Make a Car called Mary going 0 MPH.
+            Car mary = new Car("Mary");
+            mary.PrintState();
+            // Make a Car called Daisy going 75 MPH.
+            Car daisy = new Car("Daisy", 75);
+            daisy.PrintState();
+        }
+    }
+}
---
+++ 2.TypeSystem/Class/Static/Static/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+
+namespace Static
+{
+    /// <summary>
+    /// Static could be used for
+    /// • Data of a class
+    /// • Methods of a class
+    /// • Properties of a class
+    /// • A constructor
+    /// • The entire class definition
+    /// </summary>
+
+    // A simple savings account class.
+    class SavingsAccount
+    {
+        // Instance-level data.
+        public double currBalance;
+        // A static point of data.
+        public static double currInterestRate;
+        public SavingsAccount(double balance)
+        {
+            currBalance = balance;
+        }
+        // A static constructor!
+        // Runs only once before the first usage
+        // of static data and before instance constructors.
+        static SavingsAccount()
+        {
+            Console.WriteLine("In static ctor!");
+            currInterestRate = 0.04;
+        }
+        // Static members to get/set interest rate.
+        public static void SetInterestRate(double newRate)
+        { currInterestRate = newRate; }
+
+        public static double GetInterestRate()
+        { return currInterestRate; }
+    }
+
+    // Static classes can only
+    // contain static members!
+    static class TimeUtilClass
+    {
+        public static void PrintTime()
+        { Console.WriteLine(DateTime.Now.ToShortTimeString()); }
+
+        public static void PrintDate()
+        { Console.WriteLine(DateTime.Today.ToShortDateString()); }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("***** Fun with Static Data *****\n");
+            SavingsAccount s1 = new SavingsAccount(50);
+            SavingsAccount s2 = new SavingsAccount(100);
+            // Print the current interest rate.
+            Console.WriteLine("Interest Rate is: {0}", SavingsAccount.GetInterestRate());
+            // Make new object, this does NOT 'reset' the interest rate.
+            SavingsAccount s3 = new SavingsAccount(10000.75);
+            Console.WriteLine("Interest Rate is: {0}", SavingsAccount.GetInterestRate());
+            Console.ReadLine();
+
+            // This is just fine.
+            TimeUtilClass.PrintDate();
+            TimeUtilClass.PrintTime();
+        }
+    }
+}
