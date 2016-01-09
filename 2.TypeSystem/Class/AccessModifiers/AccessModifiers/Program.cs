c1c8ebce13ec8b86e62f56b6c2b4d8014f8c8ba6
wordtinker <wordtinker@yandex.ru>
2016-01-09 21:46:32 +0300

    .

---
+++ 2.TypeSystem/Class/AccessModifiers/AccessModifiers/Program.cs
@@ -0,0 +1,39 @@
+﻿
+namespace AccessModifiers
+{
+    public class CanBeVisibleAnywhere
+    {
+        private class CanBeDeclaredPrivateOnlyAsNested
+        {
+
+        }
+
+        protected class CanBeDelcaredProtectedOnlyAsNested
+        {
+
+        }
+
+        protected internal class CanBeDeclaredOnlyAsNested
+        {
+
+        }
+
+        public int CanBeAccessesAnywhere;
+        private int CanBeAccessedOnlyByDefingClass; // implicit modifier
+        protected int ForClassAndChildren;
+        internal int ForAssemblyLevelAccess;
+        protected internal int AsProtectedOrInternal;
+    }
+
+    internal class CanBeVisibleByAssemly // implicit modifier
+    {
+
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
