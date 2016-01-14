21b88e8898ed43077f0fc1224a0f9ad259e4bef4
wordtinker <wordtinker@yandex.ru>
2016-01-14 22:03:06 +0300

    .

--- 1.Basics/VarConst/VarConst/Program.cs
+++ 1.Basics/VarConst/VarConst/Program.cs
@@ -2,6 +2,17 @@
 
 namespace VarConst
 {
+    class MyMathClass
+    {
+        public const double PI = 3.14; // value is known at declaration time
+        public readonly double SuperPI; 
+
+        public MyMathClass()
+        {
+            SuperPI = PI; // readonly value can be set in constructor
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
@@ -11,6 +22,8 @@ namespace VarConst
             var someOtherTitle = title; // takes the type from title
             someOtherTitle += " !!!";
             Console.Title = someOtherTitle;
+            Console.WriteLine("{0}", MyMathClass.PI); // public const in implicitly static
+
         }
     }
 }
