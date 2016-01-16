088c6c93861709d5882dae6b8c16845590003e0b
wordtinker <wordtinker@yandex.ru>
2016-01-16 13:19:27 +0300

    .

---
+++ 3.Exceptions/Custom/Custom/CarIsDeadException.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Custom
+{
+    [Serializable]
+    public class CarIsDeadException : ApplicationException
+    {
+        public DateTime ErrorTimeStamp { get; set; }
+        public string CauseOfError { get; set; }
+
+        public CarIsDeadException() { }
+
+        // Feed message to parent constructor.
+        public CarIsDeadException(string message, string cause, DateTime time)
+            : base(message)
+        {
+            CauseOfError = cause;
+            ErrorTimeStamp = time;
+        }
+        public CarIsDeadException(string message) : base(message) { }
+        public CarIsDeadException(string message,
+                                  System.Exception inner)
+            : base(message, inner)
+        { }
+        protected CarIsDeadException(
+          System.Runtime.Serialization.SerializationInfo info,
+          System.Runtime.Serialization.StreamingContext context)
+            : base(info, context)
+        { }
+
+        // Any additional custom properties, constructors and data members...
+    }
+}
