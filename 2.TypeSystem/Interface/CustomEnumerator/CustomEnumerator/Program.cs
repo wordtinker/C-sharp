3e204a4cd8d67b5422c934a98af6d0c7d15509bd
wordtinker <wordtinker@yandex.ru>
2016-01-23 09:48:58 +0300

    .

---
+++ 2.TypeSystem/Interface/CustomEnumerator/CustomEnumerator/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+
+namespace CustomEnumerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("***** Fun with IEnumerable / IEnumerator *****\n");
+            Garage carLot = new Garage();
+
+            // Hand over each car in the collection?
+            foreach (Car c in carLot)
+            {
+                Console.WriteLine("{0} is going {1} MPH",
+                  c.PetName, c.CurrentSpeed);
+            }
+
+            // Use named iterator
+            Console.WriteLine();
+            foreach (Car c in carLot.GetTheCars(true))
+            {
+                Console.WriteLine("{0} is going {1} MPH",
+                  c.PetName, c.CurrentSpeed);
+            }
+
+            // Manually work with IEnumerator.
+            Console.WriteLine();
+            IEnumerator i = carLot.GetEnumerator();
+            i.MoveNext();
+            Car myCar = (Car)i.Current;
+            Console.WriteLine("{0} is going {1} MPH", myCar.PetName, myCar.CurrentSpeed);
+
+            Console.ReadLine();
+        }
+    }
+}
