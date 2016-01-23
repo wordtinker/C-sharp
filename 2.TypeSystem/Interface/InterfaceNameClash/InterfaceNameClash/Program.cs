508438978d22fe2ea1c53d5186eac92eca352f5f
wordtinker <wordtinker@yandex.ru>
2016-01-23 09:24:22 +0300

    .

---
+++ 2.TypeSystem/Interface/InterfaceNameClash/InterfaceNameClash/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfaceNameClash
+{
+    // Draw image to a form.
+    public interface IDrawToForm
+    {
+        void Draw();
+    }
+    // Draw to buffer in memory.
+    public interface IDrawToMemory
+    {
+        void Draw();
+    }
+    // Render to the printer.
+    public interface IDrawToPrinter
+    {
+        void Draw();
+    }
+
+    class Octagon : IDrawToForm, IDrawToMemory, IDrawToPrinter
+    {
+        // Explicitly bind Draw() implementations
+        // to a given interface.
+        void IDrawToForm.Draw()
+        {
+            Console.WriteLine("Drawing to form...");
+        }
+        void IDrawToMemory.Draw()
+        {
+            Console.WriteLine("Drawing to memory...");
+        }
+        void IDrawToPrinter.Draw()
+        {
+            Console.WriteLine("Drawing to a printer...");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("***** Fun with Interface Name Clashes *****\n");
+            // All of these invocations call the
+            // same Draw() method!
+            Octagon oct = new Octagon();
+            //oct.Draw() is no longer available w/o cast
+            IDrawToForm itfForm = (IDrawToForm)oct;
+            itfForm.Draw();
+            IDrawToPrinter itfPriner = (IDrawToPrinter)oct;
+            itfPriner.Draw();
+            IDrawToMemory itfMemory = (IDrawToMemory)oct;
+            itfMemory.Draw();
+            Console.ReadLine();
+        }
+    }
+}
---
+++ 2.TypeSystem/Interface/Interfaces/Interfaces/Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+
+namespace Interfaces
+{
+    interface ISimple
+    {
+        void CallMethod();
+    }
+
+    class Shape
+    {
+
+    }
+
+    class Triangle : Shape, ISimple
+    {
+        public void CallMethod()
+        {
+            Console.WriteLine("I can do method.");
+        }
+    }
+
+    interface ISaveable
+    {
+        void SaveToPDF();
+        void SaveToWord();
+    }
+
+    class Invoice : ISaveable
+    {
+        private object[] details;
+        public Invoice(object[] det)
+        {
+            details = det;
+        }
+
+        public void SaveToPDF()
+        {
+            Console.WriteLine("Save to PDF.");
+        }
+
+        public void SaveToWord()
+        {
+            Console.WriteLine("Save to Word.");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Invoice iv = new Invoice(new object[] { "object", "object" });
+            iv.SaveToPDF();
+            iv.SaveToWord();
+
+            Shape triangle = new Triangle(); // Watch Shape type here!
+                                             // triangle.CallMethod(); won't compile
+
+            // 1
+            try
+            {
+                Triangle tr = (Triangle)triangle;  // use cast to get an access to interface
+                tr.CallMethod();
+            }
+            catch (InvalidCastException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            // 2
+            Triangle tr2 = triangle as Triangle; // Will give null on unsucessful cast
+            if (tr2 != null)
+            {
+                tr2.CallMethod();
+            }
+            else
+            {
+                Console.WriteLine("No interface here.");
+            }
+
+            // 3
+            if (triangle is ISimple)
+            {
+                ((Triangle)triangle).CallMethod();
+            }
+            else
+            {
+                Console.WriteLine("No interface here.");
+            }
+        }
+    }
+
+}
+
