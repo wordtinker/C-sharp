4465639b91d8a8a4f1833df0c41911b7ced81645
wordtinker <wordtinker@yandex.ru>
2016-01-25 15:40:07 +0300

    .

---
+++ 4.Advanced/FinilazeDispose/FinilazeDispose/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace FinilazeDispose
+{
+    class MyResourceWrapper : IDisposable
+    {
+        // Used to determine if Dispose()
+        // has already been called.
+        private bool disposed = false;
+
+        public void Dispose()
+        {
+            // Call our helper method.
+            // Specifying "true" signifies that
+            // the object user triggered the cleanup.
+            CleanUp(true);
+
+            // Now suppress finalization.
+            GC.SuppressFinalize(this);
+        }
+
+        private void CleanUp(bool disposing)
+        {
+            // Be sure we have not already been disposed!
+            if (!this.disposed)
+            {
+                // If disposing equals true, dispose all
+                // managed resources.
+                if (disposing)
+                {
+                    // Dispose managed resources.
+                }
+                // Clean up unmanaged resources here.
+            }
+            disposed = true;
+        }
+
+        ~MyResourceWrapper()
+        {
+            Console.Beep();
+            // Call our helper method.
+            // Specifying "false" signifies that
+            // the GC triggered the cleanup.
+            CleanUp(false);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("***** Dispose() / Destructor Combo Platter *****");
+
+            // Call Dispose() manually, this will not call the finalizer.
+            MyResourceWrapper rw = new MyResourceWrapper();
+            rw.Dispose();
+
+            // Don't call Dispose(), this will trigger the finalizer
+            // and cause a beep.
+            MyResourceWrapper rw2 = new MyResourceWrapper();
+        }
+    }
+}
---
+++ 4.Advanced/LazyObjects/LazyObjects/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+
+namespace LazyObjects
+{
+    // Represents a single song. 
+    class Song
+    {
+        public string Artist { get; set; }
+        public string TrackName { get; set; }
+        public double TrackLength { get; set; }
+    }
+
+    // Represents all songs on a player.
+    class AllTracks
+    {
+        // Our media player can have a maximum
+        // of 10,000 songs.
+        private Song[] allSongs = new Song[10000];
+
+        public AllTracks()
+        {
+            // Assume we fill up the array
+            // of Song objects here.
+            Console.WriteLine("Filling up the songs!");
+        }
+    }
+
+    // The MediaPlayer has-a AllTracks object.
+    class MediaPlayer
+    {
+        // Assume these methods do something useful.
+        public void Play() { /* Play a song */ }
+        public void Pause() { /* Pause the song */ }
+        public void Stop() { /* Stop playback */ }
+
+        // private AllTracks allSongs = new AllTracks();
+        // private Lazy<AllTracks> allSongs = new Lazy<AllTracks>();
+        private Lazy<AllTracks> allSongs = new Lazy<AllTracks>(() =>
+        {
+            // If you need custom ctor call;
+            Console.WriteLine("Creating AllTracks object!");
+            return new AllTracks();
+        }
+        );
+
+        public AllTracks GetAllTracks()
+        {
+            // Return all of the songs.
+
+            // return allSongs;
+            return allSongs.Value;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("***** Fun with Lazy Instantiation *****\n");
+
+            // No allocation of AllTracks object here!
+            MediaPlayer myPlayer = new MediaPlayer();
+            myPlayer.Play();
+
+            // Allocation of AllTracks happens when you call GetAllTracks().
+            MediaPlayer yourPlayer = new MediaPlayer();
+            AllTracks yourMusic = yourPlayer.GetAllTracks();
+
+            Console.ReadLine();
+        }
+    }
+}
