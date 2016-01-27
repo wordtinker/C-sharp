2e8005b4979127c3ee137657721c3d7961f7302e
wordtinker <wordtinker@yandex.ru>
2016-01-27 12:19:26 +0300

    .

---
+++ 6.Reflection/GetType/GetType/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace GetType
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 32;
+            Type t = x.GetType(); // need instance
+            Console.WriteLine(t);
+
+            Console.WriteLine(typeof(int)); // need class
+
+            Type t2 = Type.GetType("System.Int32", false);
+            // "CarLibrary.JamesBondCar+SpyOptions" for nested types
+            Console.WriteLine(t2);
+        }
+    }
+}
---
+++ 6.Reflection/LateBindingWithDynamic/LateBindingWithDynamic/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Reflection;
+
+namespace LateBindingWithDynamic
+{
+    class Program
+    {
+        private static void AddWithReflection()
+        {
+            Assembly asm = Assembly.Load("MathLibrary");
+            try
+            {
+                // Get metadata for the SimpleMath type.
+                Type math = asm.GetType("MathLibrary.SimpleMath");
+                // Create a SimpleMath on the fly.
+                object obj = Activator.CreateInstance(math);
+                // Get info for Add.
+                MethodInfo mi = math.GetMethod("Add");
+                // Invoke method (with parameters).
+                object[] args = { 10, 70 };
+                Console.WriteLine("Result is: {0}", mi.Invoke(obj, args));
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        private static void AddWithDynamic()
+        {
+            Assembly asm = Assembly.Load("MathLibrary");
+            try
+            {
+                // Get metadata for the SimpleMath type.
+                Type math = asm.GetType("MathLibrary.SimpleMath");
+                // Create a SimpleMath on the fly.
+                dynamic obj = Activator.CreateInstance(math);
+                Console.WriteLine("Result is: {0}", obj.Add(10, 70));
+            }
+            catch (Microsoft.CSharp.RuntimeBinder.RuntimeBinderException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+        }
+    }
+}
