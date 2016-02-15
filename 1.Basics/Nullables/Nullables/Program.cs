b83a039e6dadc5fb4a1e571df2b54f0fc680a37d
wordtinker <wordtinker@yandex.ru>
2016-02-15 22:54:10 +0300

    .

--- 1.Basics/Nullables/Nullables/Program.cs
+++ 1.Basics/Nullables/Nullables/Program.cs
@@ -2,6 +2,15 @@
 
 namespace Nullables
 {
+    class MyObject
+    {
+        public class InnerObject
+        {
+            public int X { get; set; }
+        }
+
+        public InnerObject IO;
+    }
     class Program
     {
         static void Main(string[] args)
@@ -14,6 +23,10 @@ namespace Nullables
             Console.WriteLine("The value is:{0}.", nullableInt.GetValueOrDefault());
             Console.WriteLine("The value is:{0}.", nullableInt ?? 100); // Return 100 if null
             Console.WriteLine("The value is:{0}.", nullableInt.GetValueOrDefault(100)); // Same
+            // Init an object
+            MyObject mo = new MyObject();
+            // Return value of X if innerobject is not null, null otherwise
+            Console.WriteLine("The value of inner object is available: {0}", mo.IO?.X != null);
         }
     }
 }
