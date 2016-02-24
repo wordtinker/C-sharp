b86c9529e0b0f97ab25a4bcef5e3082fbe5b4a55
wordtinker <wordtinker@yandex.ru>
2016-02-24 16:24:12 +0300

    Basic Prism Core Library example

---
+++ 13.Prism/BasicPrismApp/BasicPrismApp/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows;
+
+namespace BasicPrismApp
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
---
+++ 13.Prism/BasicPrismApp/BasicPrismApp/MainViewModel.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using Prism.Mvvm;
+using Prism.Commands;
+
+namespace BasicPrismApp
+{
+    class MainViewModel: BindableBase
+    {
+        private string _firstName;
+        public string FirstName
+        {
+            get { return _firstName; }
+            set
+            {
+                SetProperty(ref _firstName, value);
+                // See ctor for another method
+                //UpdateCommand.RaiseCanExecuteChanged();
+            }
+        }
+
+        private string _lastName;
+        public string LastName
+        {
+            get { return _lastName; }
+            set
+            {
+                SetProperty(ref _lastName , value);
+                //UpdateCommand.RaiseCanExecuteChanged();
+            }
+        }
+
+        private DateTime? _lastUpdated;
+        public DateTime? LastUpdated
+        {
+            get { return _lastUpdated; }
+            set { SetProperty(ref _lastUpdated, value); }
+        }
+
+        public DelegateCommand UpdateCommand { get; set; }
+
+        public MainViewModel()
+        {
+            UpdateCommand = new DelegateCommand(Execute, CanExecute).ObservesProperty(() => FirstName).ObservesProperty(() => LastName);
+        }
+
+        private bool CanExecute()
+        {
+            return !String.IsNullOrWhiteSpace(FirstName) && !String.IsNullOrWhiteSpace(LastName);
+        }
+
+        private void Execute()
+        {
+            LastUpdated = DateTime.Now;
+        }
+
+        //public DelegateCommand UpdateCommand
+        //{
+        //    get
+        //    {
+        //        return new DelegateCommand(
+        //            () => { LastUpdated = DateTime.Now; },
+        //            () => { return !String.IsNullOrWhiteSpace(FirstName) && !String.IsNullOrWhiteSpace(LastName); })
+        //            .ObservesProperty(() => FirstName)
+        //            .ObservesProperty(() => LastName);
+        //    }
+        //}
+    }
+}
