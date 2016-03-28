05d5c2e017bb2d5588bb8b33ac5c6784b2478a0b
wordtinker <wordtinker@yandex.ru>
2016-03-28 20:49:18 +0300

    .

---
+++ 13.Prism/OpeningWindows/Ask/MainViewModel.cs
@@ -0,0 +1,48 @@
+﻿using Prism.Commands;
+using Prism.Mvvm;
+using System;
+using System.Windows.Input;
+using Prism.Interactivity.InteractionRequest;
+
+namespace Ask
+{
+    class MainViewModel : BindableBase
+    {
+        string _status;
+        public String Status
+        {
+            get { return _status; }
+            set
+            {
+                SetProperty<string>(ref _status, value);
+            }
+        }
+
+        public ICommand ConfirmationCommand { get; private set; }
+        public InteractionRequest<IConfirmation> ConfirmationRequest { get; private set; }
+
+        public MainViewModel()
+        {
+            ConfirmationRequest = new InteractionRequest<IConfirmation>();
+            ConfirmationCommand = new DelegateCommand(() =>
+            {
+                ConfirmationRequest.Raise(new Confirmation
+                {
+                    Title = "Confirmation box",
+                    Content = "Confirmation message displayed"
+                },
+                (dialog) =>
+                {
+                    if (dialog.Confirmed)
+                    {
+                        Status = "Confirmed";
+                    }
+                    else
+                    {
+                        Status = "Cancelled";
+                    }
+                });
+            });
+        }
+    }
+}
