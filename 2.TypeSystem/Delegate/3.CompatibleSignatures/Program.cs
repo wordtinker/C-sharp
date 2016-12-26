497bfef5842ab400814e2a9ff213243f3c876ada
wordtinker <wordtinker@yandex.ru>
2016-12-26 22:15:55 +0300

    Review Delegates

---
+++ 2.TypeSystem/Delegate/3.CompatibleSignatures/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CompatibleSignatures
+{
+    class Program
+    {
+        static void LogPlainEvent(object sender, EventArgs e)
+        {
+            Console.WriteLine("LogPlain");
+        }
+        static void LogKeyEvent(object sender, KeyPressEventArgs e)
+        {
+            Console.WriteLine("LogKey");
+        }
+        static void LogMouseEvent(object sender, MouseEventArgs e)
+        {
+            Console.WriteLine("LogMouse");
+        }
+
+        static void Main(string[] args)
+        {
+            Button button = new Button();
+            button.Text = "Click me";
+
+            // signature of Click: object sender, EventArgs e
+            // button.Click += LogPlainEvent;
+            // signature of KeyPress: object sender, KeyPressEventArgs e 
+            button.KeyPress += LogKeyEvent;
+            // signature of MouseClick: object sender, MouseEventArgs e
+            button.MouseClick += LogMouseEvent;
+
+            // NB! since NET 2.0
+            // we can use (object sender, EventArgs e) signature
+            // instead of (object sender, KeyPressEventArgs e)
+            button.KeyPress += LogPlainEvent;
+            button.MouseClick += LogPlainEvent;
+
+            Form form = new Form();
+            form.AutoSize = true;
+            form.Controls.Add(button);
+            Application.Run(form);
+        }
+    }
+}
---
+++ 2.TypeSystem/Delegate/4.AnonymousDelegate/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace AnonymousDelegate
+{
+    public delegate int BinaryOp(int x, int y);
+
+    public class SimpleMath
+    {
+        public static int Add(int x, int y) { return x + y; }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Simple delegate
+            BinaryOp c = SimpleMath.Add;
+            Console.WriteLine("20 + 10 is {0}", c(20, 10));
+            Console.WriteLine();
+
+            // Anonymous delegate
+            BinaryOp b = delegate (int x, int y) { return x + y;  };
+            Console.WriteLine("30 + 10 is {0}", b(30, 10));
+            // Anonymous  delegate that doesn’t depend on its parameter values
+            BinaryOp d = delegate { return 10; };
+            Console.WriteLine("10 is {0}", d(30, 10));
+        }
+    }
+}
