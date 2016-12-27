dca7a2874d1aa3837b754fc2a359e994e084e650
wordtinker <wordtinker@yandex.ru>
2016-12-27 12:56:20 +0300

    Review Iterators

---
+++ 4.Advanced/Iterators/NewWayIterator/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace NewWayIterator
+{
+    public class IterationSample : IEnumerable
+    {
+        object[] values;
+        int startingPoint;
+
+        public IterationSample(object[] values, int startingPoint)
+        {
+            this.values = values;
+            this.startingPoint = startingPoint;
+        }
+
+        public IEnumerator GetEnumerator()
+        {
+            for (int index = 0; index < values.Length; index++)
+            {
+                yield return values[(index + startingPoint) % values.Length];
+            }
+        }
+    }
+
+    class Program
+    {
+        // yield break demo
+        static IEnumerable<int> CountWithTimeLimit(DateTime limit)
+        {
+            for (int i = 1; i <= 100; i++)
+            {
+                if (DateTime.Now >= limit)
+                {
+                    yield break;
+                }
+                yield return i;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            object[] values = { "a", "b", "c", "d", "e" };
+            IterationSample collection = new IterationSample(values, 3);
+            foreach (object x in collection)
+            {
+                Console.WriteLine(x);
+            }
+
+            DateTime stop = DateTime.Now.AddSeconds(2);
+            foreach (int i in CountWithTimeLimit(stop))
+            {
+                Console.WriteLine("Received {0}", i);
+                Thread.Sleep(300);
+            }
+        }
+    }
+}
---
+++ 4.Advanced/Iterators/OldWayIterator/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections;
+
+namespace OldWayIterator
+{
+    // using non-generic collection
+    public class IterationSample : IEnumerable
+    {
+        class IterationSampleIterator : IEnumerator
+        {
+            // state
+            IterationSample parent;
+            int position;
+
+            internal IterationSampleIterator(IterationSample parent)
+            {
+                this.parent = parent;
+                position = -1;
+            }
+
+            public object Current
+            {
+                get
+                {
+                    if (position == -1 || position == parent.values.Length)
+                    {
+                        throw new InvalidOperationException();
+                    }
+                    int index = position + parent.startingPoint;
+                    index = index % parent.values.Length;
+                    return parent.values[index];
+                }
+            }
+
+            public bool MoveNext()
+            {
+                if (position != parent.values.Length)
+                {
+                    position++;
+                }
+                return position < parent.values.Length;
+            }
+
+            public void Reset()
+            {
+                position = -1;
+            }
+        }
+
+        object[] values;
+        int startingPoint;
+
+        public IterationSample(object[] values, int startingPoint)
+        {
+            this.values = values;
+            this.startingPoint = startingPoint;
+        }
+
+        public IEnumerator GetEnumerator()
+        {
+            return new IterationSampleIterator(this);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            object[] values = { "a", "b", "c", "d", "e" };
+            IterationSample collection = new IterationSample(values, 3);
+            foreach (object x in collection)
+            {
+                Console.WriteLine(x);
+            }
+        }
+    }
+}
