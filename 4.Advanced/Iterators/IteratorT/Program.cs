82d1dec10e70be8336ec6548627b873a1b572c68
wordtinker <wordtinker@yandex.ru>
2016-12-30 20:55:40 +0300

    Review iterators

---
+++ 4.Advanced/Iterators/IteratorT/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace IteratorT
+{
+    class WordIterator : IEnumerable<string>
+    {
+        private string phrase;
+
+        public WordIterator(string phrase)
+        {
+            this.phrase = phrase;
+        }
+
+        public IEnumerator<string> GetEnumerator()
+        {
+            foreach (string s in phrase.Split())
+            {
+                yield return s;
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string phrase = Console.ReadLine();
+            foreach (string s in new WordIterator(phrase))
+            {
+                Console.WriteLine($"{s}\n");
+            }
+        }
+    }
+}
