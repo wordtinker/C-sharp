235106eb840e092e4a7c8e8860a4bc602f29efaa
wordtinker <wordtinker@yandex.ru>
2017-01-07 16:28:43 +0300

    Review async

---
+++ 7.ThreadsAndAsync/10.Async/FunWithCSharpAsync/MainForm.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FunWithCSharpAsync
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        // void is only reserved for event callbacks
+        private async void btnCallMethod_Click(object sender, EventArgs e)
+        {
+            this.textBox1.Text += "Calling method async!\r\n";
+            this.textBox1.Text += await DoWorkAsync();
+        }
+
+        // Task<T> should be used with awaitable
+        private async Task<string> DoWorkAsync()
+        {
+            return await Task.Run(() =>
+            {
+                Thread.Sleep(5000);                 
+                return "Done with work!\r\n";
+            });
+        }
+
+        private async void btnVoidMethodCall_Click(object sender, EventArgs e)
+        {
+            await MethodReturningVoidAsync();
+            textBox1.Text += "Done!\r\n";
+        }
+
+        // Watch Task here! not void
+        private async Task MethodReturningVoidAsync()
+        {
+            await Task.Run(() =>
+            { /* Do some work here... */
+                Thread.Sleep(4000);
+            });
+        }
+
+        private async void btnMutliAwaits_Click(object sender, EventArgs e)
+        {
+            await Task.Run(() => { Thread.Sleep(2000); });
+            MessageBox.Show("Done with first task!");
+
+            await Task.Run(() => { Thread.Sleep(2000); });
+            MessageBox.Show("Done with second task!");
+
+            await Task.Run(() => { Thread.Sleep(2000); });
+            MessageBox.Show("Done with third task!");
+        }
+    }
+}
