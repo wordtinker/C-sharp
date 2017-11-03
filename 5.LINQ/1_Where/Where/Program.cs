804772e3ea0ab33c40ea64a4b79364b2ce258abc
wordtinker <wordtinker@yandex.ru>
2017-11-03 17:34:10 +0300

    Review LINQ

---
+++ 5.LINQ/1_Where/Where/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Where
+{
+    class Car
+    {
+        public string PetName { get; set; }
+        public string Color { get; set; }
+        public int Speed { get; set; }
+        public string Make { get; set; }
+    }
+
+    class Program
+    {
+        static void Print<T>(IEnumerable<T> seq)
+        {
+            foreach (T x in seq)
+            {
+                Console.WriteLine(x);
+            }
+            Console.WriteLine();
+        }
+
+        static List<Car> GetCars()
+        {
+            return new List<Car>() {
+                new Car{ PetName = "Daisy2", Color = "Tan", Speed = 90, Make = "BMW"},
+                new Car{ PetName = "Henry", Color = "Silver", Speed = 100, Make = "BMW"},
+                new Car{ PetName = "Daisy", Color = "Tan", Speed = 90, Make = "BMW"},
+                new Car{ PetName = "Mary", Color = "Black", Speed = 55, Make = "VW"},
+                new Car{ PetName = "Clunker", Color = "Rust", Speed = 5, Make = "Yugo"},
+                new Car{ PetName = "Melvin", Color = "White", Speed = 43, Make = "Ford"}
+            };
+        }
+
+        static void Main(string[] args)
+        {
+            int[] numbers = { 5, 4, 1, 3, 9, 8, 6, 7, 2, 0 };
+            // Can filter on element value
+            var lowNums = from n in numbers
+                          where n < 5
+                          select n;
+            Print(lowNums);
+            // Can filter on element attribute
+            var fastCars = from c in GetCars()
+                           where c.Speed >= 55
+                           select c.PetName;
+            Print(fastCars);
+            // Can filter on multiple conditions
+            // can use multiple where
+            // will be translated into where c.Speed >= 55 && c.Make == "BMW"
+            var fasterCars = from c in GetCars()
+                           where c.Speed >= 55
+                           where c.Make == "BMW"
+                           select c.PetName;
+            Print(fasterCars);
+            // Indexed where
+            string[] digits = { "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine" };
+            // returns digits whose name is shorter than their value.
+            var shortDigits = digits.Where((digit, index) => digit.Length < index);
+            Print(shortDigits);
+        }
+    }
+}
--- 5.LINQ/1_Where/Where/Properties/AssemblyInfo.cs
+++ 5.LINQ/1_Where/Where/Properties/AssemblyInfo.cs
@@ -2,34 +2,34 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
+// General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Collections")]
+[assembly: AssemblyTitle("Where")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Collections")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyProduct("Where")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("0a232488-73f8-4448-88fe-f4ca118ca9ab")]
+[assembly: Guid("a3720796-0f5b-45a5-af1e-791d780de90c")]
 
 // Version information for an assembly consists of the following four values:
 //
 //      Major Version
-//      Minor Version 
+//      Minor Version
 //      Build Number
 //      Revision
 //
-// You can specify all the values or you can default the Build and Revision Numbers 
+// You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
