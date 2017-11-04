4a9c7a7539c9d60a78c841186c1e577a1370b2fc
wordtinker <wordtinker@yandex.ru>
2017-11-04 23:11:57 +0300

    Review LINQ

---
+++ 5.LINQ/2_Select/2_Select/Program.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _2_Select
+{
+    class Car
+    {
+        public string PetName { get; set; }
+        public string Color { get; set; }
+        public int Speed { get; set; }
+        public string Make { get; set; }
+    }
+    class Program
+    {
+        static List<Car> GetCars()
+        {
+            return new List<Car>() {
+                new Car{ PetName = "Daisy2", Color = "Tan", Speed = 90, Make = "BMW"},
+                new Car{ PetName = "Henry", Color = "Silver", Speed = 100, Make = "BMW"},
+                new Car{ PetName = "Daisy", Color = "Tan", Speed = 90, Make = "BMW"},
+                new Car{ PetName = "Mary", Color = "Black", Speed = 55, Make = "VW"},
+                new Car{ PetName = "Clunker", Color = "Rust", Speed = 5, Make = "Yugo"},
+                new Car{ PetName = "Melvin", Color = "White", Speed = 43, Make = "Ford"}
+            };
+        }
+        static void MakeItPlusONe()
+        {
+            int[] numbers = { 5, 4, 1, 3, 9, 8, 6, 7, 2, 0 };
+
+            var numsPlusOne =
+                from n in numbers
+                select n + 1;
+
+            Console.WriteLine("Numbers + 1:");
+            foreach (var i in numsPlusOne)
+            {
+                Console.WriteLine(i);
+            }
+        }
+        static void MakeTheNames()
+        {
+            List<Car> products = GetCars();
+
+            var productNames =
+                from p in products
+                select p.PetName;
+
+            Console.WriteLine("Product Names:");
+            foreach (var productName in productNames)
+            {
+                Console.WriteLine(productName);
+            }
+        }
+
+        static void MapAnotherSet()
+        {
+            int[] numbers = { 5, 4, 1, 3, 9, 8, 6, 7, 2, 0 };
+            string[] strings = { "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine" };
+
+            var textNums =
+                from n in numbers
+                select strings[n];
+
+            Console.WriteLine("Number strings:");
+            foreach (var s in textNums)
+            {
+                Console.WriteLine(s);
+            }
+        }
+        static void MakeItAnon()
+        {
+            string[] words = { "aPPLE", "BlUeBeRrY", "cHeRry" };
+
+            var upperLowerWords =
+                from w in words
+                select new { Upper = w.ToUpper(), Lower = w.ToLower() };
+
+            foreach (var ul in upperLowerWords)
+            {
+                Console.WriteLine("Uppercase: {0}, Lowercase: {1}", ul.Upper, ul.Lower);
+            }
+        }
+        static void SelectIndexed()
+        {
+            int[] numbers = { 5, 4, 1, 3, 9, 8, 6, 7, 2, 0 };
+
+            var numsInPlace = numbers.Select((num, index) => new { Num = num, InPlace = (num == index) });
+
+            Console.WriteLine("Number: In-place?");
+            foreach (var n in numsInPlace)
+            {
+                Console.WriteLine("{0}: {1}", n.Num, n.InPlace);
+            }
+        }
+        static void SelectMany()
+        {
+            int[] numbersA = { 0, 2, 4, 5, 6, 8, 9 };
+            int[] numbersB = { 1, 3, 5, 7, 8 };
+
+            var pairs =
+                from a in numbersA
+                from b in numbersB
+                where a < b
+                select new { a, b };
+
+            Console.WriteLine("Pairs where a < b:");
+            foreach (var pair in pairs)
+            {
+                Console.WriteLine("{0} is less than {1}", pair.a, pair.b);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            // Can project elements into modified itself
+            MakeItPlusONe();
+            // Can project elements into attibutes
+            MakeTheNames();
+            // Can project elements into element of another set
+            MapAnotherSet();
+            // Can project elements into anonymous type
+            MakeItAnon();
+            // Can project elements with index
+            SelectIndexed();
+            // Can project elemenets of two seqs as nested loop
+            SelectMany();
+        }
+    }
+}
---
+++ 5.LINQ/3_TakeAndSkip/3_TakeAndSkip/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3_TakeAndSkip
+{
+    class Program
+    {
+        static void TakeThree()
+        {
+            int[] numbers = { 5, 4, 1, 3, 9, 8, 6, 7, 2, 0 };
+            var first3Numbers = numbers.Take(3);
+
+            Console.WriteLine("First 3 numbers:");
+            foreach (var n in first3Numbers)
+
+            {
+                Console.WriteLine(n);
+            }
+        }
+        static void SkipFour()
+        {
+            int[] numbers = { 5, 4, 1, 3, 9, 8, 6, 7, 2, 0 };
+            var allButFirst4Numbers = numbers.Skip(4);
+
+            Console.WriteLine("All but first 4 numbers:");
+            foreach (var n in allButFirst4Numbers)
+            {
+                Console.WriteLine(n);
+            }
+        }
+        static void NotUntilSix()
+        {
+            int[] numbers = { 5, 4, 1, 3, 9, 8, 6, 7, 2, 0 };
+            var firstNumbersLessThan6 = numbers.TakeWhile(n => n < 6);
+
+            Console.WriteLine("First numbers less than 6:");
+            foreach (var n in firstNumbersLessThan6)
+            {
+                Console.WriteLine(n);
+            }
+        }
+        static void NotUntilIndexed()
+        {
+            int[] numbers = { 5, 4, 1, 3, 9, 8, 6, 7, 2, 0 };
+            var firstSmallNumbers = numbers.TakeWhile((n, index) => n >= index);
+
+            Console.WriteLine("First numbers not less than their position:");
+            foreach (var n in firstSmallNumbers)
+            {
+                Console.WriteLine(n);
+            }
+        }
+        static void SkipUntil()
+        {
+            int[] numbers = { 5, 4, 1, 3, 9, 8, 6, 7, 2, 0 };
+            var allButFirst3Numbers = numbers.SkipWhile(n => n % 3 != 0);
+
+            Console.WriteLine("All elements starting from first element divisible by 3:");
+
+            foreach (var n in allButFirst3Numbers)
+            {
+                Console.WriteLine(n);
+            }
+        }
+        static void Main(string[] args)
+        {
+            // Can take only first several items
+            TakeThree();
+            // Can skip several items
+            SkipFour();
+            // Can take as long as condition is met
+            NotUntilSix();
+            // Can do that with index
+            NotUntilIndexed();
+            // Can skip until condition is met
+            SkipUntil();
+        }
+    }
+}
