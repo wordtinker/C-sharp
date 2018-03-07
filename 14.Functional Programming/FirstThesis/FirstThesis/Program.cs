e8c11cebb30d03999a03c190c791e003baae2efe
wordtinker <wordtinker@yandex.ru>
2018-03-07 13:41:37 +0300

    FP1

---
+++ 14.Functional Programming/FirstThesis/FirstThesis/Program.cs
@@ -0,0 +1,28 @@
+﻿// watch 'static' here
+using System;
+using System.Threading.Tasks;
+using static System.Console;
+using static System.Linq.Enumerable;
+
+namespace FirstThesis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // We need this elements for FP
+            // - functiona as first order class values
+            // - avoid state mutations
+            var nums = Range(-10_000, 20_001).Reverse().ToList();
+            Action task1 = () => WriteLine(nums.Sum());
+            Action task2 = () => { nums.Sort(); WriteLine(nums.Sum()); };
+            Action task3 = () => WriteLine(nums.OrderBy(x => x).Sum());
+
+            // this will mess the state of nums.
+            Parallel.Invoke(task1, task2);
+            WriteLine("-------");
+            // this will not
+            Parallel.Invoke(task1, task3);
+        }
+    }
+}
