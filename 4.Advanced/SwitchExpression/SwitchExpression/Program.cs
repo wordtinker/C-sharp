b3308f1448ab5b24ae4922f23a9e41ec650a7999
wordtinker <wordtinker@yandex.ru>
2019-04-18 17:48:36 +0300

    C# 8.0

---
+++ 4.Advanced/SwitchExpression/SwitchExpression/Program.cs
@@ -0,0 +1,113 @@
+﻿using System;
+
+namespace SwitchExpression
+{
+    public class RGBColor
+    {
+        private int v1;
+        private int v2;
+        private int v3;
+
+        public RGBColor(int v1, int v2, int v3)
+        {
+            this.v1 = v1;
+            this.v2 = v2;
+            this.v3 = v3;
+        }
+    }
+
+    public enum Rainbow
+    {
+        Red,
+        Orange,
+        Yellow,
+        Green,
+        Blue,
+        Indigo,
+        Violet
+    }
+
+    class Address
+    {
+        public string State { get; set; }
+    }
+
+    public class Point
+    {
+        public int X { get; }
+        public int Y { get; }
+
+        public Point(int x, int y) => (X, Y) = (x, y);
+
+        public void Deconstruct(out int x, out int y) =>
+            (x, y) = (X, Y);
+    }
+
+    public enum Quadrant
+    {
+        Unknown,
+        Origin,
+        One,
+        Two,
+        Three,
+        Four,
+        OnBorder
+    }
+
+    class Program
+    {
+        public static RGBColor FromRainbow(Rainbow colorBand) =>
+            // switch expression
+            colorBand switch
+            {
+                Rainbow.Red => new RGBColor(0xFF, 0x00, 0x00),
+                Rainbow.Orange => new RGBColor(0xFF, 0x7F, 0x00),
+                Rainbow.Yellow => new RGBColor(0xFF, 0xFF, 0x00),
+                Rainbow.Green => new RGBColor(0x00, 0xFF, 0x00),
+                Rainbow.Blue => new RGBColor(0x00, 0x00, 0xFF),
+                Rainbow.Indigo => new RGBColor(0x4B, 0x00, 0x82),
+                Rainbow.Violet => new RGBColor(0x94, 0x00, 0xD3),
+                _ => throw new ArgumentException(message: "invalid enum value", paramName: nameof(colorBand)),
+            };
+
+        public static decimal ComputeSalesTax(Address location, decimal salePrice) =>
+            location switch
+            {
+                // note property here
+                { State: "WA" } => salePrice * 0.06M,
+                { State: "MN" } => salePrice * 0.75M,
+                { State: "MI" } => salePrice * 0.05M,
+                // other cases removed for brevity...
+                _ => 0M
+            };
+
+        public static string RockPaperScissors(string first, string second) =>
+            (first, second) switch // note tuple here
+            {
+                ("rock", "paper") => "rock is covered by paper. Paper wins.",
+                ("rock", "scissors") => "rock breaks scissors. Rock wins.",
+                ("paper", "rock") => "paper covers rock. Paper wins.",
+                ("paper", "scissors") => "paper is cut by scissors. Scissors wins.",
+                ("scissors", "rock") => "scissors is broken by rock. Rock wins.",
+                ("scissors", "paper") => "scissors cuts paper. Scissors wins.",
+                (_, _) => "tie"
+            };
+
+        public static Quadrant GetQuadrant(Point point) =>
+            point switch
+            {
+                (0, 0) => Quadrant.Origin, // note Deconstruct call here
+                var (x, y) when x > 0 && y > 0 => Quadrant.One, // note when
+                var (x, y) when x < 0 && y > 0 => Quadrant.Two,
+                var (x, y) when x < 0 && y < 0 => Quadrant.Three,
+                var (x, y) when x > 0 && y < 0 => Quadrant.Four,
+                var (_, _) => Quadrant.OnBorder,
+                _ => Quadrant.Unknown
+            };
+
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
