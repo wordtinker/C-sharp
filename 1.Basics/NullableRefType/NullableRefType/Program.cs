267203a1fd6a1b7d98b4d144cf756e10ffa06260
wordtinker <wordtinker@yandex.ru>
2019-04-18 20:35:46 +0300

    C# 8.0

---
+++ 1.Basics/NullableRefType/NullableRefType/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace NullableRefType
+{
+
+    class Item
+    {
+        public void Do()
+        {
+
+        }
+    }
+
+    class Forgiving
+    {
+        public string Title { get; set; } = default!; // ! suppresses compiler warning
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            Item cantBeNull = new Item();
+            Item? canBeNull = null; // no warning
+
+            cantBeNull.Do();
+            canBeNull?.Do(); // warning
+
+            // Add this to .csproj file
+            // < LangVersion > 8.0 </ LangVersion >
+            // < NullableReferenceTypes > true </ NullableReferenceTypes >
+            // < NullableContextOptions > enable </ NullableContextOptions >
+
+
+        }
+    }
+}
