d9f1f6068462e3d1b371f67275ab98c9fad55077
wordtinker <wordtinker@yandex.ru>
2019-04-18 18:37:06 +0300

    C# 8.0

---
+++ 4.Advanced/NewUsing/NewUsing/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace NewUsing
+{
+    class Program
+    {
+        static void NewWriteLinesToFile(IEnumerable<string> lines)
+        {
+            using var file = new System.IO.StreamWriter("WriteLines2.txt");
+            foreach (string line in lines)
+            {
+                // If the line doesn't contain the word 'Second', write the line to the file.
+                if (!line.Contains("Second"))
+                {
+                    file.WriteLine(line);
+                }
+            }
+            // file is disposed here
+        }
+
+        static void OldWriteLinesToFile(IEnumerable<string> lines)
+        {
+            using (var file = new System.IO.StreamWriter("WriteLines2.txt"))
+            {
+                foreach (string line in lines)
+                {
+                    // If the line doesn't contain the word 'Second', write the line to the file.
+                    if (!line.Contains("Second"))
+                    {
+                        file.WriteLine(line);
+                    }
+                }
+            } // file is disposed here
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
