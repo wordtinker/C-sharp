1fd76891196cc9f1e7ec4e4732f40e4178d641f4
wordtinker <wordtinker@yandex.ru>
2019-04-19 14:28:46 +0300

    C# 8.0

---
+++ 1.Basics/Ranges/Ranges/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Ranges
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            var words = new string[]
+            {
+                            // index from start    index from end
+                "The",      // 0                   ^9
+                "quick",    // 1                   ^8
+                "brown",    // 2                   ^7
+                "fox",      // 3                   ^6
+                "jumped",   // 4                   ^5
+                "over",     // 5                   ^4
+                "the",      // 6                   ^3
+                "lazy",     // 7                   ^2
+                "dog"       // 8                   ^1
+            };
+
+            Console.WriteLine($"The last word is {words[^1]}"); // dog
+            var quickBrownFox = words[1..4];
+            var lazyDog = words[^2..^0];
+            var allWords = words[..]; // contains "The" through "dog".
+            var firstPhrase = words[..4]; // contains "The" through "fox"
+            var lastPhrase = words[6..]; // contains "the, "lazy" and "dog"
+            Range phrase = 1..4;
+            var text = words[phrase];
+        }
+    }
+}
---
+++ 7.ThreadsAndAsync/11.AsynchronousStream/AsynchronousStream/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace AsynchronousStream
+{
+    class Program
+    {
+
+        // IProgress<int> progress is no longer needed
+        // CancellationToken can be passed as parameter to
+        // GenerateSeq(CancellationToken) is needed
+        static async IAsyncEnumerable<int> GenerateSeq()
+        {
+            for (int i = 0; i < 20; i++)
+            {
+                await Task.Delay(100);
+                yield return i;
+            }
+        }
+
+        static async Task Main()
+        {
+            await foreach (var item in GenerateSeq())
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
